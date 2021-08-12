b733b87abf2c17f1fce5c4dce009e0f885ae7ab3
Denchik <soft478569@gmail.com>
2021-08-13 01:25:19 +0600

    how do get xml data

--- test.py
+++ test.py
@@ -1,1 +1,13 @@
-print('hello world'.upper())
+from flask import Flask, request, jsonify
+import requests
+import xmltodict
+
+app=Flask(__name__)
+
+@app.route("/", methods=['GET', 'POST'])
+def parse_xml():
+    content_dict = xmltodict.parse(request.data)
+    return jsonify(content_dict)
+
+if __name__=='__main__':
+    app.run(host="0.0.0.0")
